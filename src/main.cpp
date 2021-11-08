bd919feb31e70b77524d27d1b0a55f9651f9732b
Barney Wilks <barneywilks1@gmail.com>
2021-11-08 00:35:40 +0000

    Fix clang_frontend.cpp and frontend.h encoding
    Was saved as a weird encoding, which is why git thinks they are binary files
    (UCS-2 BE BOM or something??? no idea how that happened really). They are now
    saved as unicode.
    
    The actual contents changed is just experimenting with the clang APIs more...

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "frontend.h"
 
-int main()
+int main(int argc, const char** argv)
 {
 	Helix::Frontend::Initialise();
+
+	Helix::Frontend::Run(argc, argv);
+
 	Helix::Frontend::Shutdown();
 
 	return 0;
