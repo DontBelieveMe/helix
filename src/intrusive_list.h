beedebe8575e7bf0442fe22e10cdbac61be80a3d
Barney Wilks <barneywilks1@gmail.com>
2021-11-15 17:01:58 +0000

    Add intrusive_list ADT

---
+++ src/intrusive_list.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include <assert.h>
+
+namespace Helix
+{
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	class intrusive_list_node
+	{
+	public:
+		intrusive_list_node() = default;
+		virtual ~intrusive_list_node() = default;
+
+		intrusive_list_node(const intrusive_list_node& other) = delete;
+		intrusive_list_node& operator=(const intrusive_list_node& other) = delete;
+
+		intrusive_list_node(intrusive_list_node&& other) = default;
+		intrusive_list_node& operator=(intrusive_list_node&& other) = default;
+
+		void set_next(intrusive_list_node* next) { m_next = next; }
+		void set_prev(intrusive_list_node* prev) { m_prev = prev; }
+
+		intrusive_list_node* get_next() const { return m_next; }
+		intrusive_list_node* get_prev() const { return m_prev; }
+
+	private:
+		intrusive_list_node* m_next = nullptr;
+		intrusive_list_node* m_prev = nullptr;
+	};
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	template <typename T>
+	class intrusive_list;
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	template <typename T>
+	class intrusive_list_iterator
+	{
+		friend intrusive_list<T>;
+
+	public:
+		intrusive_list_iterator(T* node)
+			: m_node(node) { }
+
+		bool operator==(const intrusive_list_iterator<T>& other) const
+			{ return m_node == other.m_node; }
+
+		bool operator!=(const intrusive_list_iterator<T>& other) const
+			{ return m_node != other.m_node; }
+
+		T& operator*()
+			{ return *m_node; }
+
+		T* operator->()
+			{ return m_node; }
+
+		intrusive_list_iterator<T>& operator++()
+			{ m_node = static_cast<T*>(m_node->get_next()); return *this; }
+
+		intrusive_list_iterator<T>& operator--()
+			{ m_node = static_cast<T*>(m_node->get_prev()); return *this; }
+
+		intrusive_list_iterator<T> operator++(int)
+			{ return intrusive_list_iterator<T>(m_node->get_next()); }
+
+		intrusive_list_iterator<T> operator--(int)
+			{ return intrusive_list_iterator<T>(m_node->get_prev()); }
+
+	private:
+		T* m_node;
+	};
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	template <typename T>
+	class intrusive_list
+	{
+	public:
+		using iterator       = intrusive_list_iterator<T>;
+		using const_iterator = intrusive_list_iterator<const T>;
+
+		intrusive_list()
+		{
+			m_sentinel.set_prev(&m_sentinel);
+			m_sentinel.set_next(&m_sentinel);
+		}
+
+		bool empty() const { return begin() == end(); }
+
+		iterator       begin() { return iterator(static_cast<T*>(m_sentinel.get_next())); }
+		iterator       end()   { return iterator(static_cast<T*>(&m_sentinel));           }
+
+		const_iterator begin() const { return const_iterator(static_cast<const T*>(m_sentinel.get_next())); }
+		const_iterator end()   const { return const_iterator(static_cast<const T*>(&m_sentinel)); }
+
+		iterator insert_before(const iterator& where, T* value)
+		{
+			T* node = where.m_node;
+			T* prev = static_cast<T*>(node->get_prev());	
+
+			prev->set_next(value);
+			node->set_prev(value);
+
+			value->set_prev(prev);
+			value->set_next(node);
+
+			return iterator(value);
+		}
+
+		void push_back(T* value) { this->insert_before(end(), value); }
+
+		T& back() { return *static_cast<T*>(m_sentinel.get_prev()); }
+
+	private:
+		intrusive_list_node m_sentinel;
+	};
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
---
+++ src/tests/test_intrusive_list.cpp
@@ -0,0 +1,140 @@
+#include "../intrusive_list.h"
+
+#include "catch.hpp"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct MyInteger : public Helix::intrusive_list_node
+{
+	MyInteger(int v) : value(v) { }
+	int value = 0xcafe;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+struct ScopeAllocator
+{
+public:
+	~ScopeAllocator()
+	{
+		std::destroy_n(values.begin(), values.size());
+	}
+
+	T* Parent(T* p)
+	{
+		values.push_back(p);
+		return p;
+	}
+
+private:
+	std::vector<T*> values;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST_CASE("Empty intrusive_list", "[intrusive_list]")
+{
+	Helix::intrusive_list<MyInteger> ints;
+
+	REQUIRE(ints.begin() == ints.end());
+	REQUIRE(ints.empty());
+
+	int count = 0;
+
+	for (const MyInteger& integer : ints)
+	{
+		count++;
+	}
+
+	REQUIRE(count == 0);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST_CASE("Single push_back", "[intrusive_list]")
+{
+	ScopeAllocator<MyInteger> alloc;
+
+	Helix::intrusive_list<MyInteger> ints;
+	ints.push_back(alloc.Parent(new MyInteger(123)));
+
+	const MyInteger& v = *ints.begin();
+
+	REQUIRE(!ints.empty());
+	REQUIRE(v.value == 123);
+
+	int sum = 0;
+	for (const MyInteger& integer : ints)
+	{
+		sum += integer.value;
+	}
+
+	REQUIRE(sum == 123);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST_CASE("Multiple push back", "[intrusive_list]")
+{
+	ScopeAllocator<MyInteger> alloc;
+
+	Helix::intrusive_list<MyInteger> ints;
+	
+	MyInteger* a = alloc.Parent(new MyInteger(1));
+	MyInteger* b = alloc.Parent(new MyInteger(2));
+	MyInteger* c = alloc.Parent(new MyInteger(3));
+	MyInteger* d = alloc.Parent(new MyInteger(4));
+
+	ints.push_back(a); REQUIRE(ints.back().value == 1);
+	ints.push_back(b); REQUIRE(ints.back().value == 2);
+	ints.push_back(c); REQUIRE(ints.back().value == 3);
+	ints.push_back(d); REQUIRE(ints.back().value == 4);
+
+	std::vector<int> expected = { 1, 2, 3, 4 };
+
+	int index = 0; bool matches = true;
+	
+	for (const MyInteger& i : ints)
+	{
+		matches &= (i.value == expected[index]);
+		index++;
+	}
+
+	REQUIRE(matches);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST_CASE("Muliple random insertions (insert_before)", "[intrusive_list]")
+{
+	ScopeAllocator<MyInteger> alloc;
+
+	Helix::intrusive_list<MyInteger> ints;
+
+	MyInteger* a = alloc.Parent(new MyInteger(1));
+	MyInteger* b = alloc.Parent(new MyInteger(2));
+	MyInteger* c = alloc.Parent(new MyInteger(3));
+	MyInteger* d = alloc.Parent(new MyInteger(4));
+
+	ints.push_back(a);
+	ints.push_back(b);
+
+	// it points to 'b' (we want to insert a node before 'b')
+	Helix::intrusive_list<MyInteger>::iterator it = ints.begin(); ++it;
+
+	ints.insert_before(it, c);
+	ints.insert_before(ints.begin(), d);
+
+	std::vector<int> expected = { 4, 1, 3, 2 };
+	int index = 0; bool matches = true;
+	for (const MyInteger& i : ints)
+	{
+		matches &= (i.value == expected[index]);
+		index++;
+	}
+
+	REQUIRE(matches);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
