387d4e0e1712ee52fa280c1727ae2348b4236d39
Barney Wilks <barneywilks1@gmail.com>
2021-11-21 03:06:07 +0000

    Make the test file a bit simpler
    Just reduce it to a constant integer addition and return it.
    This is a very very simple example and isn't really realistic but
    generating this correctly will be a start.

--- testsuite/f.c
+++ testsuite/f.c
@@ -1,19 +1,5 @@
-typedef int my_integer;
-typedef my_integer mi;
-
-struct hello_world
-{
-	int v;
-};
-
-typedef struct hello_world shw;
-
-mi add(mi a, mi b) {
-	return a + b;
-}
-
-void addg(mi a, int b, struct hello_world c, shw  d) { }
-
 int main()
 {
+	int a = 1 + 2;
+	return a;
 }
