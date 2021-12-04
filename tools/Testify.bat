c74b7ce236601b6a50d751ad5c4a2ccad4acf24a
Barney Wilks <barneywilks1@gmail.com>
2021-12-04 15:29:08 +0000

    Update Testify runner script to look for the optimized version first
    If the optimized version cannot be found then fallback on the debug version.
    If that doesn't exist then exit out with an error message

--- tools/Testify.bat
+++ tools/Testify.bat
@@ -1,2 +1,23 @@
 @echo off
-%~dp0/Testify/bin/Debug/netcoreapp3.1/Testify.exe %*
+
+set COMMON_PREFIX=%~dp0Testify\bin
+set COMMON_POSTFIX=netcoreapp3.1\Testify.exe
+
+set DEBUG_TESTIFY=%COMMON_PREFIX%\Debug\%COMMON_POSTFIX%
+set RELEASE_TESTIFY=%COMMON_PREFIX%\Release\%COMMON_POSTFIX%
+
+if exist %RELEASE_TESTIFY% (
+    set TESTIFY_EXE=%RELEASE_TESTIFY%
+) else (
+    if exist %DEBUG_TESTIFY% (
+        set TESTIFY_EXE=%DEBUG_TESTIFY%
+    ) else (
+        echo Cannot find Testify.exe
+        exit
+    )
+)
+
+echo Found Testify at '%TESTIFY_EXE%'
+echo.
+
+%TESTIFY_EXE% %*
