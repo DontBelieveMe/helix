071db59147d7d6164d1ef0d3b1959ee09abd2e20
Barney Wilks <barneywilks1@gmail.com>
2021-12-07 01:58:00 +0000

    Add script for building LLVM in release mode
    Hoping this gives some compiler performance back (compiles taking 1.5-3s is not great...)

--- tools/BuildLLVM_Release.bat
+++ tools/BuildLLVM_Release.bat
@@ -3,16 +3,17 @@ pushd .
 cd %~dp0\..\external\llvm-project
 
 echo Configuring CMake...
-cmake -Sllvm -Bbuild-debug -A x64 -Thost=x64 ^
+cmake -Sllvm -Bbuild-release -A x64 -Thost=x64 ^
     -DLLVM_ENABLE_PROJECTS=clang ^
     -DLLVM_TARGETS_TO_BUILD=X86 ^
-    -DCMAKE_BUILD_TYPE=Debug ^
+    -DCMAKE_BUILD_TYPE=Release ^
     -DLLVM_ENABLE_ASSERTIONS=ON ^
     -DLLVM_ENABLE_ZLIB=OFF
 
 echo Building Binaries...
-cmake --build build-debug ^
+cmake --build build-release ^
       --target clangAST ^
+      --config Release ^
 clangASTMatchers ^
 clangAnalysis ^
 clangBasic ^
@@ -28,7 +29,7 @@ clangTooling ^
 clangToolingCore ^
 clangToolingSyntax
 
-xcopy build-debug\Debug\lib\*.lib ..\..\lib\bin\ /S /Y
+xcopy build-debug\Release\lib\*.lib ..\..\lib\bin\ /S /Y
 
 xcopy build-debug\include\*.h ..\..\lib\include\ /S /y
 xcopy build-debug\include\*.def ..\..\lib\include\ /S /y
