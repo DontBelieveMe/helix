d4d992bdb90b8d1caa6f1754e3df16f2daf1b805
Barney Wilks <barneywilks1@gmail.com>
2021-12-22 02:26:29 +0000

    Add *int*_t definitions to stdint.h

---
+++ src/libc/include/stdint.h
@@ -0,0 +1,19 @@
+#ifndef _STDINT_H_
+#define _STDINT_H_
+
+typedef char      int8_t;
+typedef short     int16_t;
+typedef int       int32_t;
+typedef long long int64_t;
+
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+typedef unsigned int       uint32_t;
+typedef unsigned long long uint64_t;
+
+#define INT8_WIDTH  (8)
+#define INT16_WIDTH (16)
+#define INT32_WIDTH (32)
+#define INT64_WIDTH (64)
+
+#endif
