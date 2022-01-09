2d059a8d0035d610894228908b62de133e1aa390
Barney Wilks <barneywilks1@gmail.com>
2022-01-09 02:48:55 +0000

    Some cleaning up

--- src/match.cpp
+++ src/match.cpp
@@ -4,10 +4,7 @@
  */
 
 #include "match.h"
-#include "options.h"
 #include "module.h"
-#include "print.h"
-#include "helix.h"
 #include "mir.h"
 
 using namespace Helix;
@@ -16,17 +13,13 @@ using namespace Helix;
 
 void MachineExpander::Execute(Module* mod)
 {
-	VirtualRegisterName* t0 = VirtualRegisterName::Create(BuiltinTypes::GetInt32());
-	VirtualRegisterName* t1 = VirtualRegisterName::Create(BuiltinTypes::GetInt32());
-	VirtualRegisterName* t2 = VirtualRegisterName::Create(BuiltinTypes::GetInt32());
-
 	for (Function* fn : mod->functions()) {
 		for (BasicBlock& bb : fn->blocks()) {
-
 			BasicBlock::iterator it = bb.begin();
+
 			while (it != bb.end()) {
 				Instruction*        old  = &(*it);
-				MachineInstruction* insn = ARMv7::Expand(old);// ARMv7::CreateAdd_r32r32(t0, t1, t2);
+				MachineInstruction* insn = ARMv7::Expand(old);
 
 				it = bb.Where((Instruction*) it->get_next());
 
