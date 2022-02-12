ba7b3ca0c8823d8db365f87ef2e5ad2a56b818c8
Barney Wilks <barneywilks1@gmail.com>
2022-02-12 17:02:51 +0000

    Update tools\TestAll.bat script
    Some things have changed since this was first written

--- tools/TestAll.bat
+++ tools/TestAll.bat
@@ -2,7 +2,7 @@
 
 pushd.
 cd %~dp0\..
-cmake --build vs2019 --target LibHelixTests Helix --config Release
+cmake --build vs2022 --target HelixCoreTests HelixCompiler --config Release
 call tools\Testify
-vs2019\Release\libhelix-tests.exe
+vs2022\src\tests\Release\libhelix-tests.exe
 popd
