9ad0bc5fe7846585650fb300fee21f0ea789f018
Barney Wilks <barneywilks1@gmail.com>
2022-02-12 16:56:27 +0000

    Add physical register initialisation to the unit tests

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -2,9 +2,12 @@
 #include "catch.hpp"
 
 #include "../types.h"
+#include "../target-info-armv7.h"
 
 int main( int argc, char* argv[] ) {
 	Helix::BuiltinTypes::Init();
+	Helix::PhysicalRegisters::Init();
+
 	int result = Catch::Session().run( argc, argv );
 	return result;
 }
