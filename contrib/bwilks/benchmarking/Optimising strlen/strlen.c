394c190311773fbcf51a2d3da43a65f735ff8be1
Barney Wilks <barneywilks1@gmail.com>
2022-02-17 02:08:11 +0000

    Add a load of notes

---
+++ contrib/bwilks/benchmarking/Optimising strlen/strlen.c
@@ -0,0 +1,12 @@
+int benchmark_main() {
+	char* c = "dKxLIlmNLlMSKucIlaprXsQUpSWdyIZwtDCNSoICZDrCcQsFXNwTXemQobCGnWk"
+	          "mIYgEUrqgUZoOdiHaueNbyAPuvOyeZKRTvTbNOYFbSqUwoerMuOwCDwFMjZCwBm"
+	          "uBnuQkKmDnqOAhjizxyBVwgasyWAMSiXSqQBJwdWRoSfGFhUhIXDVBZZnvprXlJ"
+	          "eVMTLBHXMESDRPbDoCHaqByuZLFszUciRzOwzsswyDhSTHVRtnzvBevTXNreKJCaiP";
+
+	int l = 0;
+	while (c[l] != '\0')
+		l++;
+
+	return l;
+}
