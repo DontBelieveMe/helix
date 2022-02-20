4f7d987a29488646045f32993ecdf77ae16b8d0a
Barney Wilks <barneywilks1@gmail.com>
2022-02-20 02:03:09 +0000

    Add some empty test files for BasicBlock, IR, MIR and types

---
+++ src/tests/test-basic-block.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file test-basic-block.cpp
+ * @author Barney Wilks
+ */
+
+ /* Helix Core Includes */
+#include "..\basic-block.h"
+
+/* Testing Library Includes */
+#include "catch.hpp"
+
+using namespace Helix;
+
+/*********************************************************************************************************************/
+
+TEST_CASE("", "[]")
+{
+
+}
+
+/*********************************************************************************************************************/
