3911bd7ccb0d883c82873e88b007c32e45218320
Barney Wilks <barneywilks1@gmail.com>
2022-02-20 02:08:42 +0000

    Add some tests for IsMachineTerminator

--- src/tests/test-mir.cpp
+++ src/tests/test-mir.cpp
@@ -13,9 +13,32 @@ using namespace Helix;
 
 /*********************************************************************************************************************/
 
-TEST_CASE("", "[]")
+TEST_CASE("IsMachineTerminator", "[MIR]")
 {
+	SECTION("Where given opcode is a terminator instruction")
+	{
+		REQUIRE(Helix::IsMachineTerminator(ARMv7::Beq));
+		REQUIRE(Helix::IsMachineTerminator(ARMv7::Blt));
+		REQUIRE(Helix::IsMachineTerminator(ARMv7::Bgt));
+		REQUIRE(Helix::IsMachineTerminator(ARMv7::Bge));
+		REQUIRE(Helix::IsMachineTerminator(ARMv7::Bgt));
+		REQUIRE(Helix::IsMachineTerminator(ARMv7::Bne));
+		REQUIRE(Helix::IsMachineTerminator(ARMv7::Ret));
+		REQUIRE(Helix::IsMachineTerminator(ARMv7::Br));
+	}
 
+	SECTION("Where given opcode is not a terminator instruction (not a LLIR instruction)")
+	{
+		REQUIRE(!Helix::IsMachineTerminator(HLIR::ConditionalBranch));
+		REQUIRE(!Helix::IsMachineTerminator(HLIR::UnconditionalBranch));
+	}
+
+	SECTION("Where given opcode is not a terminator instruction (another LLIR instruction)")
+	{
+		REQUIRE(!Helix::IsMachineTerminator(ARMv7::Movwge));
+		REQUIRE(!Helix::IsMachineTerminator(ARMv7::Add_r32i32));
+		REQUIRE(!Helix::IsMachineTerminator(ARMv7::Cmpi));
+	}
 }
 
 /*********************************************************************************************************************/
