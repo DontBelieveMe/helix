1ea369e970737665d3eb1c5fec51bec50ac6fb32
Barney Wilks <barneywilks1@gmail.com>
2022-02-20 02:13:36 +0000

    Add some unit tests for type_cast

--- src/tests/test-types.cpp
+++ src/tests/test-types.cpp
@@ -13,9 +13,32 @@ using namespace Helix;
 
 /*********************************************************************************************************************/
 
-TEST_CASE("", "[]")
+TEST_CASE("type_cast returns NULL given a NULL input", "[Types]")
 {
+	const IntegerType* result = Helix::type_cast<IntegerType>(nullptr);
 
+	REQUIRE(result == nullptr);
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("type_cast returns NULL where the given pointer is not of the given type", "[Types]")
+{
+	const FunctionType* f = FunctionType::Create(BuiltinTypes::GetVoidType(), {});
+
+	const IntegerType* result = Helix::type_cast<IntegerType>(f);
+
+	REQUIRE(result == nullptr);
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("type_cast returns valid pointer where the given input pointer is of the right type", "[Types]")
+{
+	const FunctionType* f = FunctionType::Create(BuiltinTypes::GetVoidType(), {});
+	const FunctionType* a = Helix::type_cast<FunctionType>(f);
+
+	REQUIRE(a == f);
 }
 
 /*********************************************************************************************************************/
