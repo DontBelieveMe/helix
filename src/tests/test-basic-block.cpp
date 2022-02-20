8a2aab6c3157fbe3de3f1f9608ea967aa6a45752
Barney Wilks <barneywilks1@gmail.com>
2022-02-20 02:22:15 +0000

    Add some tests for BasicBlock

--- src/tests/test-basic-block.cpp
+++ src/tests/test-basic-block.cpp
@@ -5,6 +5,7 @@
 
  /* Helix Core Includes */
 #include "..\basic-block.h"
+#include "..\mir.h"
 
 /* Testing Library Includes */
 #include "catch.hpp"
@@ -13,9 +14,84 @@ using namespace Helix;
 
 /*********************************************************************************************************************/
 
-TEST_CASE("", "[]")
+TEST_CASE("BasicBlock::Append", "[BasicBlock]")
 {
+	BasicBlock* bb = BasicBlock::Create();
 
+	RetInsn* ret = Helix::CreateRet();
+	bb->Append(ret);
+
+	REQUIRE(bb->GetCountInstructions() == 1);
+	REQUIRE(bb->GetLast() == ret);
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("BasicBlock::HasTerminator with HLIR terminator", "[BasicBlock]")
+{
+	BasicBlock* bb = BasicBlock::Create();
+
+	bb->Append(Helix::CreateRet());
+
+	REQUIRE(bb->HasTerminator());
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("BasicBlock::HasTerminator with LLIR terminator", "[BasicBlock]")
+{
+	BasicBlock* bb = BasicBlock::Create();
+	bb->Append(ARMv7::CreateRet());
+
+	REQUIRE(bb->HasTerminator());
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("BasicBlock::HasTerminator with a last non terminator instruction", "[BasicBlock]")
+{
+	BasicBlock* bb = BasicBlock::Create();
+	bb->Append(Helix::CreateBinOp(HLIR::IAdd, nullptr, nullptr, nullptr));
+
+	REQUIRE(!bb->HasTerminator());
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("BasicBlock::HasTerminator with no instructions in the block", "[BasicBlock]")
+{
+	BasicBlock* bb = BasicBlock::Create();
+	REQUIRE(!bb->HasTerminator());
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("BasicBlock::IsEmpty when the block has no instructions", "[BasicBlock]")
+{
+	BasicBlock* bb = BasicBlock::Create();
+
+	REQUIRE(bb->IsEmpty());
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("BasicBlock::IsEmpty when the block is not empty", "[BasicBlock]")
+{
+	BasicBlock* bb = BasicBlock::Create();
+	bb->Append(Helix::CreateRet());
+
+	REQUIRE(!bb->IsEmpty());
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("BasicBlock::IsEmpty when all instructions have been removed from the block", "[BasicBlock]")
+{
+	BasicBlock* bb = BasicBlock::Create();
+	bb->Append(Helix::CreateRet());
+	bb->Remove(bb->begin());
+
+	REQUIRE(bb->IsEmpty());
 }
 
 /*********************************************************************************************************************/
