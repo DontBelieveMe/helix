9906bb34b48ca61277b4450b2c1a20a4ab293dec
Barney Wilks <barneywilks1@gmail.com>
2022-02-22 12:07:03 +0000

    Add some more unit tests for Types

--- src/tests/test-types.cpp
+++ src/tests/test-types.cpp
@@ -42,3 +42,73 @@ TEST_CASE("type_cast returns valid pointer where the given input pointer is of t
 }
 
 /*********************************************************************************************************************/
+
+TEST_CASE("Creating a new ArrayType", "[Types]")
+{
+	const ArrayType* type = ArrayType::Create(10, BuiltinTypes::GetInt32());
+
+	REQUIRE(type->GetCountElements() == 10);
+	REQUIRE(type->GetBaseType() == BuiltinTypes::GetInt32());
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("Creating a new named StructType", "[Types]")
+{
+	StructType::FieldList fields { BuiltinTypes::GetInt32() };
+	const StructType* type = StructType::Create("hello", fields);
+
+	REQUIRE(strcmp(type->GetName(), "hello") == 0);
+	REQUIRE(type->GetCountFields() == 1);
+	REQUIRE(type->GetField(0) == BuiltinTypes::GetInt32());
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("Creating a new unnamed StructType", "[Types]")
+{
+	StructType::FieldList fields { BuiltinTypes::GetInt32() };
+	const StructType* type = StructType::Create(fields);
+
+	REQUIRE(type->GetCountFields() == 1);
+	REQUIRE(type->GetField(0) == BuiltinTypes::GetInt32());
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("Creating two different unnamed StructTypes with different names", "[Types]")
+{
+	StructType::FieldList fields { BuiltinTypes::GetInt32() };
+
+	const StructType* a = StructType::Create(fields);
+	const StructType* b = StructType::Create(fields);
+
+	REQUIRE(strcmp(a->GetName(), b->GetName()) != 0);
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("Creating a new FunctionType", "[Types]")
+{
+	const FunctionType::ParametersList params { BuiltinTypes::GetInt32() };
+
+	const FunctionType* type = FunctionType::Create(BuiltinTypes::GetVoidType(), params);
+
+	REQUIRE(type->GetReturnType() == BuiltinTypes::GetVoidType());
+	REQUIRE(type->GetParameters() == params);
+}
+
+/*********************************************************************************************************************/
+
+TEST_CASE("FunctionType::CopyWithDifferentReturnType", "[Types]")
+{
+	const FunctionType::ParametersList params { BuiltinTypes::GetInt32() };
+	const FunctionType* old = FunctionType::Create(BuiltinTypes::GetVoidType(), params);
+
+	const FunctionType* type = old->CopyWithDifferentReturnType(BuiltinTypes::GetInt32());
+
+	REQUIRE(type->GetParameters() == params);
+	REQUIRE(type->GetReturnType() == BuiltinTypes::GetInt32());
+}
+
+/*********************************************************************************************************************/
