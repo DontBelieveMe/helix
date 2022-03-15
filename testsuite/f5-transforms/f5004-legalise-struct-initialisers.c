e0407c1134dffe5fbffb6cab4187d6147bac4d79
Barney Wilks <barneywilks1@gmail.com>
2022-03-15 21:15:26 +0000

    Fix test f5004
    Adjust so that it returns a test integer rather than a struct

--- testsuite/f5-transforms/f5004-legalise-struct-initialisers.c
+++ testsuite/f5-transforms/f5004-legalise-struct-initialisers.c
@@ -2,7 +2,7 @@ struct E { int c; };
 
 struct ST { int a; int b; int c; struct E d; };
 
-struct ST get_st() {
+int main() {
   struct ST a = { 10, 20, 30, { 40 } };
-  return a;
+  return a.d.c;
 }
